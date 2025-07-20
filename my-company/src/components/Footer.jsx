7ce8441a65cfe154de2c059eccf34b509857e076
Ibk-okiki <ibukunoluwaadelokiki@gmail.com>
2025-07-20 20:53:18 +0100

    Update Footer.jsx

--- my-company/src/components/Footer.jsx
+++ my-company/src/components/Footer.jsx
@@ -1,1 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ textAlign: 'center', padding: '10px', backgroundColor: '#eee' }}>
+      <p>&copy; 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+export default Footer;
 
