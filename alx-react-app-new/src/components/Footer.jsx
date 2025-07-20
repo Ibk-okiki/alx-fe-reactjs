e9449bbad05f2b8f691e4249176fec783b056057
Ibk-okiki <ibukunoluwaadelokiki@gmail.com>
2025-07-20 20:24:37 +0100

    Create Footer.jsx

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+// src/components/Footer.jsx
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#333', color: '#fff', textAlign: 'center', padding: '15px', marginTop: '20px' }}>
+      <p>&copy; 2025 My Favorite Cities</p>
+    </footer>
+  );
+}
+
+export default Footer;
