4ddc6b04a80f006cede5ea27733880349ce3dc2b
Ibk-okiki <ibukunoluwaadelokiki@gmail.com>
2025-07-20 20:28:25 +0100

    Update App.js

--- alx-react-app-new/src/App.js
+++ alx-react-app-new/src/App.js
@@ -1,13 +1,18 @@
 // src/App.js
+
+export default App;
+
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import Counter from './components/Counter'; // 🆕 Import
 
 function App() {
   return (
     <div>
       <Header />
       <MainContent />
+      <Counter /> {/* 🆕 Add counter here */}
       <Footer />
     </div>
   );
