63aa973015636cf1840d52e7dcdddcf40ba4a4cf
Ibk-okiki <ibukunoluwaadelokiki@gmail.com>
2025-08-04 00:07:01 +0100

    Create App.js

---
+++ recipe-sharing-app/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RecipeList from './RecipeList';
+import RecipeDetails from './RecipeDetails';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/recipes/:id" element={<RecipeDetails />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
