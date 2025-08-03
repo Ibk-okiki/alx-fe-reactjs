b2bb2be1b0e0269dc44f64e6f3658e02dd026830
Ibk-okiki <ibukunoluwaadelokiki@gmail.com>
2025-08-04 00:02:41 +0100

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
+import HomePage from './components/HomePage'; // assume this lists recipes
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="App">
         <h1>🍲 Recipe Sharing App</h1>
         <Routes>
+          <Route path="/" element={<HomePage />} />
           <Route path="/" element={
             <>
               <AddRecipeForm />
