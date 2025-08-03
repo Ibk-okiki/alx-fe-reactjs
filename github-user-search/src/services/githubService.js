cabfc7b752e44f549dbc1722c7899af9663e624f
Ibk-okiki <ibukunoluwaadelokiki@gmail.com>
2025-08-03 23:40:33 +0100

    Update githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -21,4 +21,13 @@ export const fetchUserData = async (username) => {
     throw new Error('User not found');
   }
 };
+export const searchUsers = async (username, location = '', minRepos = '') => {
+  let query = `${username}`;
 
+  if (location) query += `+location:${location}`;
+  if (minRepos) query += `+repos:>=${minRepos}`;
+
+  const url = `https://api.github.com/search/users?q=${query}`;
+  const response = await axios.get(url);
+  return response.data.items;
+};
